80391d992568eda6661d8d6bf2d80e4d45294dbf
so-suke <attoutekimasshu@gmail.com>
2020-02-25 02:11:03 +0900

    a

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>hello2</p>
+        <p>hello3</p>
         <a
           className="App-link"
           href="https://reactjs.org"
