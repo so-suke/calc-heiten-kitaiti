095b1566d4c052c223e08b5269247c304c906fb3
so-suke <attoutekimasshu@gmail.com>
2020-02-25 01:29:54 +0900

    Updates

---
+++ precache-manifest.0eef41cfc1599e2d42dea27ebb04485b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d9b8ee1194055a24d83f30fd4b73d2e",
+    "url": "/calc-heiten-kitaiti/index.html"
+  },
+  {
+    "revision": "d63824482fa2c987a201",
+    "url": "/calc-heiten-kitaiti/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "dc1490fbf2a4096a7307",
+    "url": "/calc-heiten-kitaiti/static/js/2.b037f3ca.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/calc-heiten-kitaiti/static/js/2.b037f3ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d63824482fa2c987a201",
+    "url": "/calc-heiten-kitaiti/static/js/main.07937c34.chunk.js"
+  },
+  {
+    "revision": "37239035206946803fdc",
+    "url": "/calc-heiten-kitaiti/static/js/runtime-main.71b158fb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/calc-heiten-kitaiti/static/media/logo.5d5d9eef.svg"
+  }
+]);
